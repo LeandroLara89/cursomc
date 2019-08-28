8717974b3984e88ad314cbcafd6f907547000000
Leandro Lara <leandrodasilvalara@yahoo.com.br>
2019-08-28 17:36:36 -0300

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/leandrolara/cursomc/domain/Pedido.java
+++ src/main/java/com/leandrolara/cursomc/domain/Pedido.java
@@ -35,7 +35,7 @@ public class Pedido implements Serializable {
 	@OneToOne(cascade=CascadeType.ALL, mappedBy="pedido")
 	private Pagamento pagamento;
 
-	@JsonManagedReference
+	
 	@ManyToOne
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
